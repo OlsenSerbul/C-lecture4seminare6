ee259606f2ecd1ce687f5775376acbc90059b563
Olga Serbul <Olsen.zika@mail.ru>
2022-11-17 19:52:38 +0300

    практика_лекция_project01_закрашивание

--- Examples/Практика_лекция/Progect01/Program.cs
+++ Examples/Практика_лекция/Progect01/Program.cs
@@ -37,3 +37,24 @@ void PrintImage(int[,] image)
     }
 }
 PrintImage(pic);
+
+// создаем метод для заполнения котура 
+// арг-ты:координаты строка  столбец
+void FillImage(int row,  int col)
+{
+    //пиксель:строка,столбец
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+PrintImage(pic);
+// задаем старт(должен быть внутри картинки)
+FillImage(13, 13);
+PrintImage(pic);
+
