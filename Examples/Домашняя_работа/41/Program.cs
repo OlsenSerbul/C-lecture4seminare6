9d3bcc301c6433a1c35898b207b62d8d66e9f6a1
Olga Serbul <Olsen.zika@mail.ru>
2022-11-21 17:28:19 +0300

    домашняя_работа_41

---
+++ Examples/Домашняя_работа/41/Program.cs
@@ -0,0 +1,23 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте,
+//  сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Сколько чисел вы будете вводить: М = ");
+int Length = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[Length];
+for (int i = 0; i < Length; i++ )
+{
+  Console.Write($"Введите число под номером  {i + 1}: ");
+  array[i] = Convert.ToInt32(Console.ReadLine());
+}
+int count = 0;
+for (int i = 0; i < Length; i++)
+{
+    if(array[i] > 0)
+    {
+        count++;
+    }
+}
+Console.WriteLine($"Вы ввели {count} числа больше 0");
+
+
