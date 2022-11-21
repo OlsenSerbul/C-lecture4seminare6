e3115d86019281d725491f1b1201a875c1d25d0f
Olga Serbul <Olsen.zika@mail.ru>
2022-11-21 16:34:59 +0300

    практика_Семинар_task03

---
+++ Examples/Практика_семинар/Task02/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход три числа 
+// и проверяет, может ли существовать треугольник со сторонами такой длины
+// Теорема о неравенстве треугольника: каждая сторона треугольника меньше суммы двух других сторон
+
+Console.Write("Введите число А:  ");
+int A = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:  ");
+int B = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число C:  ");
+int C = Convert.ToInt32(Console.ReadLine());
+
+bool GetTR (int A, int B, int C)
+{
+    if( A <= B + C && B <= A + C && C <= B + A)
+    {
+        return true;
+    }
+    else 
+    return false;
+}
+Console.WriteLine(GetTR(A, B, C));
+
---
+++ Examples/Практика_семинар/Task03/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая будет преобразовывать
+//  десятичное число в двоичное.
+
+Console.Write("Введите число:  ");
+int X = Convert.ToInt32(Console.ReadLine());
+
+string GetDvoich(int X)
+{
+    string rez = "";
+    int Num = 0;
+    while(X != 0)
+    {
+        X = X / 2;
+        Num = X % 2;
+        rez = Num.ToString() + rez;
+    
+    }
+    return rez;
+
+}
+string rez = GetDvoich(X);
+
+Console.WriteLine(rez);
